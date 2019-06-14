1ec69585974c6470024f4876ddbceec3e590b260
samiusik <sam.com007@yandex.ru>
2019-06-14 10:39:28 +0300

    lesson4

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,31 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)одит или когда-либ
+
+
+Answer.create([{ title: 'Correct' }, {title: 'Uncorrect'}])
+Category.create(
+  [
+    { title: 'HTML' },
+    { title: 'Ruby' },
+    { title: 'SEO'}
+  ]
+)
+Question.create(
+  [
+    { title: 'your questions', level: '1' },
+    { title: 'What is WP?', level: '0' },
+    { title: 'What is Ruby?', level: '2' }
+  ]
+)
+Test.create(
+  [
+    { title: 'Ruby', level: '2', category_id: Category.where(title: "Ruby").first.id },
+    { title: 'HTML', level: '1', category_id: Category.where(title: "HTML").first.id },
+    { title: 'SEO', level: '0', category_id: Category.where(title: "SEO").first.id }
+  ]
+)
+User.create(
+  { title: 'Alexandr' }
+)
