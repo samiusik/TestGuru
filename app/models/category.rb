c1f7940969bdfb4267a8ba4fbea4f0b86c4dbada
samiusik <sam.com007@yandex.ru>
2019-07-04 00:07:45 +0300

    v1

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_many :tests
+
+  scope :ordered_by_title, -> { order(title: :asc) }
+
+  validates :title, presence: :true
+  
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,16 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   belongs_to :author, class_name: 'User'
+
+  scope :test_easy, -> { where(level: 0..1).order(created_at: :desc) }
+  scope :test_medium, -> { where(level: 2..4).order(created_at: :desc) }
+  scope :test_hard, -> { where(level: 5..Float::INFINITY).order(created_at: :desc) }
+
+  validates :title, presence :true
+                    uniqueness: true
+
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.names_with_category(category)
     joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
   end
